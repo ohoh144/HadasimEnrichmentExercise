c736b58b2b3ee1af698dc451e6d84523b3d7c10b
ohoh144@gmail.com <ohoh144@gmail.com>
2025-09-05 08:59:20 +0300

    Added Q1 and Q2 folders

---
+++ ExerciseCSharp/PartB/Q1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace Q1
+{
+    class Program
+    {
+        static int GetNumLength(int num)
+        {
+            return (int)Math.Log10(num) + 1;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetNumLength(85258));
+            //Console.ReadLine(); //for seeing thr result so the consol won't shot down
+        }
+         
+    }
+}
